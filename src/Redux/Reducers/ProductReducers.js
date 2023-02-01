9061acedffe672d02fb76a8796bbe7b7b3eab013
kat <katrinashui1992@gmail.com>
2023-02-01 23:21:21 +0800

    change the reducer

--- src/Redux/Reducers/ProductReducers.js
+++ src/Redux/Reducers/ProductReducers.js
@@ -19,10 +19,10 @@ export const productListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_SUCCESS:
       return {
         loading: false,
-        // pages: action.payload.pages,
-        // page: action.payload.page,
-        // products: action.payload.products,
-        products:action.payload
+        pages: action.payload.pages,
+        page: action.payload.page,
+        products: action.payload.products,
+        // products:action.payload
       };
     case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
